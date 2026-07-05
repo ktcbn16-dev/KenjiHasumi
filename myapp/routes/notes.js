bb06fbd4b29eb41da33092e9934c2302f47bc9ab
Kenji Hasumi <k.tcbn@MacBook-Pro.local>
2026-07-05 17:18:26 +0900

    Add cat API

--- myapp/routes/notes.js
+++ myapp/routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const url = "ここにMongoDBの接続情報を入れる";
+const url = "++++++++++++";
 const client = new MongoClient(url);
 
 router.get('/', async (req, res) => {
