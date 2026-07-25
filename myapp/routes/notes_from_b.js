5b61897b20806c5da295829d6cd9906dc598f2b8
Kenji Hasumi <k.tcbn16@icloud.com>
2026-07-26 01:07:47 +0900

    add env

--- myapp/routes/notes_from_b.js
+++ myapp/routes/notes_from_b.js
@@ -1,22 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const url = process.env.MONGODB_URI;
-const client = new MongoClient(url);
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
 
 router.get('/', async (req, res) => {
   // データベース、コレクションを指定
   const database = client.db('notes');
   const notes = database.collection('notes');
 
-  // idが1のドキュメントを取得
-  const query = { id: 1 };
-  const note = await notes.findOne(query);
+
+  // 全てのドキュメントを取得
+  const note = await notes.find({}).toArray();
 
   res.json(note);
-});
+})
 
 module.exports = router;
